c08e94ec9fd5cc157df768daa1d9cf4e0bc27a5d
Ali Naseri <mo.ali.naseri@gmail.com>
2024-04-24 20:03:59 +0330

    feat: add drug brands and generic drugs

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 # Register your models here.
 
 from django.contrib import admin
-from home.models import PatientCase, Relative, OtherSupporter
+from home.models import PatientCase, Relative, OtherSupporter, GenericDrug, DrugBrand
 
 class RelativeInline(admin.TabularInline):
     model = Relative
@@ -16,3 +16,13 @@ class PatientCaseAdmin(admin.ModelAdmin):
         RelativeInline,
         OtherSupporterInline
     ]
+
+@admin.register(DrugBrand)
+class DrugBrandAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in DrugBrand._meta.fields]
+    inlines = []
+
+@admin.register(GenericDrug)
+class GenericDrugAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in GenericDrug._meta.fields]
+    inlines = []
--- home/models.py
+++ home/models.py
@@ -182,3 +182,30 @@ class OtherSupporter(models.Model):
     class Meta:
         verbose_name = _('Other Supporter')
         verbose_name_plural = _('Other Supporters')
+
+class GenericDrug(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+    created_at = models.DateField(auto_now_add=True, verbose_name=_('Created At'))
+    modified_at = models.DateField(auto_now=True, verbose_name=_('Modified At'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Generic Drug')
+        verbose_name_plural = _('Generic Drugs')
+
+class DrugBrand(models.Model):
+    brand_name = models.CharField(max_length=255, verbose_name=_('Brand Name'))
+    country = models.CharField(max_length=30, verbose_name=_('Country'))
+    address = models.CharField(max_length=255, verbose_name=_('Address'))
+    phone = models.CharField(max_length=20, verbose_name=_('Phone'))
+    created_at = models.DateField(auto_now_add=True, verbose_name=_('Created At'))
+    modified_at = models.DateField(auto_now=True, verbose_name=_('Modified At'))
+
+    def __str__(self):
+        return self.brand_name
+
+    class Meta:
+        verbose_name = _('Drug Brand')
+        verbose_name_plural = _('Drug Brands')
