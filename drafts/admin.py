5658b5af985d236e1002f7b4d06aef57110509b1
Ali Naseri <mo.ali.naseri@gmail.com>
2024-07-07 23:07:06 +0330

    fix: add autocomplete for draft sections

--- drafts/admin.py
+++ drafts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 
 from drafts.models import DraftSupporter, DraftDrug, Draft
 
@@ -6,12 +7,21 @@ from drafts.models import DraftSupporter, DraftDrug, Draft
 class DraftSupporterInline(admin.TabularInline):
     model = DraftSupporter
 
-
 class DraftDrugInline(admin.TabularInline):
     model = DraftDrug
+    autocomplete_fields = ['drug_brand', 'generic_drug']
 
 
 @admin.register(Draft)
 class DraftAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Draft._meta.fields]
+    fields = ['patient_case']
+    readonly_fields = ['created_at']
     inlines = [DraftDrugInline, DraftSupporterInline]
+
+    autocomplete_fields = ['patient_case']
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.creator = request.user
+        super().save_model(request, obj, form, change)
--- drugs/admin.py
+++ drugs/admin.py
@@ -6,10 +6,12 @@ from drugs.models import DrugBrand, GenericDrug
 @admin.register(DrugBrand)
 class DrugBrandAdmin(admin.ModelAdmin):
     list_display = [field.name for field in DrugBrand._meta.fields]
+    search_fields = ['brand_name']
     inlines = []
 
 
 @admin.register(GenericDrug)
 class GenericDrugAdmin(admin.ModelAdmin):
     list_display = [field.name for field in GenericDrug._meta.fields]
+    search_fields = ['name']
     inlines = []
